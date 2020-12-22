8c89241b1bbeabdc1c5319c40bbd418408181789
Florent Burdairon <florent.burdairon@imft.fr>
2020-12-22 15:56:52 +0100

    Solve Day 3.2

--- Day3/toboggan_trajectory.py
+++ Day3/toboggan_trajectory.py
@@ -36,11 +36,17 @@ def convert_input_to_matrix( input_list ):
 def repeat_forest( slope, input_forest_matrix ):
     Lx = input_forest_matrix.shape[1]
     Ly = input_forest_matrix.shape[0]
-    delta_y_max = Ly-1 # end-start
-    delta_x_max = delta_y_max*int(abs(slope[0]/slope[1]))
-    x_mult = int(delta_x_max/Lx) +1
-    #print(f"The forest must be repeated {x_mult} times in the direction x")
-    repeated_forest = np.tile( input_forest_matrix, x_mult )
+    dx = slope[0] ; dy = slope[1]
+    delta_x = Lx-1 ; delta_y = Ly-1
+    if dy/dx < delta_y/delta_x:
+        delta_y_max = delta_y
+        delta_x_max = delta_y_max* dx/dy
+        x_mult = int(delta_x_max/Lx) +1
+        print(f"The forest must be repeated {x_mult} times in the direction x")
+        repeated_forest = np.tile( input_forest_matrix, x_mult )
+    else:
+        print("No need to repeat horizontally")
+        repeated_forest = input_forest_matrix
     return repeated_forest
 
 def get_path_into_forest( slope, repeated_forest ):
@@ -52,15 +58,23 @@ def get_path_into_forest( slope, repeated_forest ):
         x_pos += slope[0]
         y_pos += slope[1]
     nb_trees = sum(path)
-    print(f"Number of trees encountered on the path : {nb_trees}")
     return nb_trees
 
 def solve_part1( slope, input_list ):
-    Lx, Ly = forest_size( input_list )
-    print(f"The dimensions of the input are : {Lx} horizontal units, {Ly} vertical units")
     input_forest_matrix = convert_input_to_matrix( input_list )
     repeated_forest = repeat_forest( slope, input_forest_matrix )
     nb_encountered_trees = get_path_into_forest( slope, repeated_forest )
+    return nb_encountered_trees
+
+def solve_part2( list_of_slopes, input_list ):
+    answer = 1
+    for slope in list_of_slopes:
+        print("-------------------------------------------------------")
+        print(f"slope = ({slope[0]}, {slope[1]})")
+        nb_of_trees = solve_part1( slope, input_list )
+        answer = answer * nb_of_trees
+    print("-------------------------------------------------------")
+    print(f"Answer to part 2 is {answer}")
 
 
 # Convention : x increasing from left to the right, y increasing from top to bottom, origin is top left
@@ -69,4 +83,9 @@ print(f"Remember : the slope is {slope[0]} horizontal, {slope[1]} vertical")
 filename = "puzzle.input"
 dirname = "./"
 input_list = read_puzzle_input( dirname+filename )
-solve_part1( slope, input_list )
+Lx, Ly = forest_size( input_list )
+print(f"The dimensions of the input are : {Lx} horizontal units, {Ly} vertical units")
+print(f"Part 1 : number of trees encountered on the path = {solve_part1( slope, input_list )}")
+
+list_of_slopes = np.array([[1,1], [3,1], [5,1], [7,1], [1,2]])
+solve_part2( list_of_slopes, input_list )
